f4f453daa7214f47097d469d5f2f7815d0edb5a3
Shanshan Li <shanshan.li@booking.com>
2021-06-07 23:01:03 -0700

    create tweet mode and create init migration file and add the tweet unit test case

---
+++ tweets/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+from utils.time_helpers import utc_now
+
+# Create your models here.
+class Tweet(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        help_text='who posts this tweet',
+        # verbose_name=u'谁发了这个帖子',
+    )
+    content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def hours_to_now(self):
+        return (utc_now()- self.created_at).seconds // 3600
+
+    def __str__(self):
+        # 这里是你执行 print(tweet instance) 的时候会显示的内容
+        return f'{self.created_at} {self.user}: {self.content}'
